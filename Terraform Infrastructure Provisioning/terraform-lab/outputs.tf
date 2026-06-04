1b1c67ac3caa11ce0733696c0b988b84cc2145b0
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 20:37:31 -0700

    Create outputs.tf

---
+++ Terraform Infrastructure Provisioning/terraform-lab/outputs.tf
@@ -0,0 +1,14 @@
+# TODO: Output the web server container ID
+output "web_container_id" {
+  # Reference the container resource
+}
+
+# TODO: Output the network name
+output "network_name" {
+  # Display the created network name
+}
+
+# TODO: Output the access URL
+output "web_url" {
+  # Construct URL using localhost and port variable
+}
