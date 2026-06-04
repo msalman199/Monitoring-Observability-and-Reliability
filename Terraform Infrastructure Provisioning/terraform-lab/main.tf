532bf6f86d09ab85eb20472e406d02517c4c7052
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 20:38:16 -0700

    Update main.tf

--- Terraform Infrastructure Provisioning/terraform-lab/main.tf
+++ Terraform Infrastructure Provisioning/terraform-lab/main.tf
@@ -36,3 +36,50 @@ resource "docker_container" "app_server" {
   # Use a different image (e.g., httpd, redis)
   # Configure networking and dependencies
 }
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_network" "app_network" {
+  name   = "terraform-network"
+  driver = "bridge"
+}
+
+resource "docker_volume" "app_data" {
+  name = var.volume_name
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web_server" {
+  name  = "terraform-nginx"
+  image = docker_image.nginx.image_id
+  
+  ports {
+    internal = 80
+    external = var.web_port
+  }
+  
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+  
+  volumes {
+    volume_name    = docker_volume.app_data.name
+    container_path = "/usr/share/nginx/html"
+  }
+  
+  restart = var.restart_policy
+}
+
+
