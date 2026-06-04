c79c9f429560eac5c2bdcf8877aaa8bdb249dc2b
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 20:15:34 -0700

    Create test_calculator.py

---
+++ Automated Pipeline Creation /cicd-demo/test_calculator.py
@@ -0,0 +1,31 @@
+"""
+Unit tests for calculator module
+"""
+import unittest
+from calculator import add, subtract, multiply, divide
+
+class TestCalculator(unittest.TestCase):
+    
+    def test_add(self):
+        """Test addition function"""
+        # TODO: Add test cases for addition
+        pass
+    
+    def test_subtract(self):
+        """Test subtraction function"""
+        # TODO: Add test cases for subtraction
+        pass
+    
+    def test_multiply(self):
+        """Test multiplication function"""
+        # TODO: Add test cases for multiplication
+        pass
+    
+    def test_divide(self):
+        """Test division function"""
+        # TODO: Add test cases for division
+        # TODO: Test division by zero
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
