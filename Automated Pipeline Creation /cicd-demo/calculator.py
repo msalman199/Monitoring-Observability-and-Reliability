619cf59425dcd5fcf80cf290d725101cbb63d02a
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 20:15:01 -0700

    Create calculator.py

---
+++ Automated Pipeline Creation /cicd-demo/calculator.py
@@ -0,0 +1,23 @@
+"""
+Simple calculator module for CI/CD demonstration
+"""
+
+def add(a, b):
+    """Add two numbers"""
+    # TODO: Implement addition
+    pass
+
+def subtract(a, b):
+    """Subtract b from a"""
+    # TODO: Implement subtraction
+    pass
+
+def multiply(a, b):
+    """Multiply two numbers"""
+    # TODO: Implement multiplication
+    pass
+
+def divide(a, b):
+    """Divide a by b"""
+    # TODO: Implement division with zero check
+    pass
